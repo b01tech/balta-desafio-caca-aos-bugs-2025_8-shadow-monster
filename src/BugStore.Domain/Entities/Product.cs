389cd2dc8e65eef3f5baeddcaf838acdaf7161c7
b01tech <bruno01.tech@gmail.com>
2025-10-29 08:55:25 -0300

    feat(domain): add product entity

---
+++ src/BugStore.Domain/Entities/Product.cs
@@ -0,0 +1,30 @@
+using BugStore.Exception.ExceptionMessages;
+using BugStore.Exception.ProjectException;
+
+namespace BugStore.Domain.Entities;
+public class Product
+{
+    public Guid Id { get; init; }
+    public string Title { get; private set; } = string.Empty;
+    public string Description { get; private set; } = string.Empty;
+    public string Slug { get; private set; } = string.Empty;
+    public decimal Price { get; private set; }
+
+    protected Product() { }
+
+    public Product(string title, string description, string slug, decimal price)
+    {
+        ValidatePrice(price);
+        Id = Guid.CreateVersion7();
+        Title = title;
+        Description = description;
+        Slug = slug;
+        Price = price;
+    }
+
+    private void ValidatePrice(decimal price)
+    {
+        if (price < 0)       
+            throw new OnValidationException(ResourceExceptionMessage.PRICE_NEGATIVE);       
+    }
+}
