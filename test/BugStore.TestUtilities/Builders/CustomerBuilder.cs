ad93a965449978216903059c16863e26df6683d6
b01tech <bruno01.tech@gmail.com>
2025-10-29 10:28:26 -0300

    test(utilities): add customer builder for test data generation

---
+++ test/BugStore.TestUtilities/Builders/CustomerBuilder.cs
@@ -0,0 +1,19 @@
+using Bogus;
+using BugStore.Domain.Entities;
+
+namespace BugStore.TestUtilities.Builders;
+
+public static class CustomerBuilder
+{
+    private static readonly Faker _faker = new("pt_BR");
+    public static Customer Build(string? name = null, string? email = null, string? phone = null, DateTime? birthDate = null)
+    {
+        return new Customer(
+            name: name ?? _faker.Person.FullName,
+            email: email ?? _faker.Person.Email,
+            phone: phone ?? _faker.Phone.PhoneNumber("(##) #####-####"),
+            birthDate: birthDate ?? _faker.Person.DateOfBirth.Date
+        );
+    }
+
+}
