b084a6f5615905d79f303f651ce789ca7e55ac41
b01tech <bruno01.tech@gmail.com>
2025-10-29 11:27:00 -0300

    test: add OrderBuilder for test

---
+++ test/BugStore.TestUtilities/Builders/OrderBuilder.cs
@@ -0,0 +1,39 @@
+using Bogus;
+using BugStore.Domain.Entities;
+
+namespace BugStore.TestUtilities.Builders
+{
+    public static class OrderBuilder
+    {
+        private static readonly Faker _faker = new("pt_BR");
+
+        public static Order Build(Guid? customerId = null)
+        {
+            var order = new Order(customerId ?? Guid.CreateVersion7());
+            return order;
+        }
+
+        public static Order BuildWithLines(Guid? customerId = null, int lineCount = 2)
+        {
+            var order = Build(customerId);
+
+            for (int i = 0; i < lineCount; i++)
+            {
+                var productId = Guid.CreateVersion7();
+                var quantity = _faker.Random.Int(1, 10);
+                var price = _faker.Random.Decimal(10, 1000);
+
+                order.AddLine(productId, quantity, price);
+            }
+
+            return order;
+        }
+
+        public static Order BuildWithSpecificLine(Guid? customerId = null, Guid? productId = null, int quantity = 1, decimal price = 100m)
+        {
+            var order = Build(customerId);
+            order.AddLine(productId ?? Guid.CreateVersion7(), quantity, price);
+            return order;
+        }
+    }
+}
