0fa72984be2121e8d05dfaf2673ea5bc3971e8f5
b01tech <bruno01.tech@gmail.com>
2025-10-30 20:35:31 -0300

    feat(repositories): add product and order repositories to dependency injection

--- src/BugStore.Infra/Extensions/InfrastructureExtensions.cs
+++ src/BugStore.Infra/Extensions/InfrastructureExtensions.cs
@@ -9,23 +9,34 @@ namespace BugStore.Infra.Extensions
 {
     public static class InfrastructureExtensions
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration
+        )
         {
             AddDataBaseContext(services, configuration);
             AddRepositories(services);
             return services;
         }
 
-        private static void AddDataBaseContext(IServiceCollection services, IConfiguration configuration)
+        private static void AddDataBaseContext(
+            IServiceCollection services,
+            IConfiguration configuration
+        )
         {
             services.AddDbContext<BugStoreDbContext>(options =>
-                options.UseNpgsql(configuration.GetConnectionString("PostgreSql")));
+                options.UseNpgsql(configuration.GetConnectionString("PostgreSql"))
+            );
         }
 
         private static void AddRepositories(IServiceCollection services)
         {
             services.AddScoped<ICustomerReadOnlyRepository, CustomerReadOnlyRepository>();
             services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddScoped<IProductReadOnlyRepository, ProductReadOnlyRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+            services.AddScoped<IOrderReadOnlyRepository, OrderReadOnlyRepository>();
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
     }
