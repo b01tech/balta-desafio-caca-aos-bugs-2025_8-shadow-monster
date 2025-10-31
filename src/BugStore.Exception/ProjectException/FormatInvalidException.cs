ec0c81a15b694e5aee755b75b45c0105006de60e
b01tech <bruno01.tech@gmail.com>
2025-10-31 11:07:52 -0300

    feat(exception): add FormatInvalidException

---
+++ src/BugStore.Exception/ProjectException/FormatInvalidException.cs
@@ -0,0 +1,9 @@
+namespace BugStore.Exception.ProjectException
+{
+    public class FormatInvalidException : BugStoreException
+    {
+        public FormatInvalidException(string message) : base(message)
+        {
+        }
+    }
+}
