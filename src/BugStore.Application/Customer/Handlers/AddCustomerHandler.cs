c2ea4d14ce10c9e23919e9762578d3a5f1706086
b01tech <bruno01.tech@gmail.com>
2025-10-30 22:03:02 -0300

    feat(customer): add validation to customer handlers

--- src/BugStore.Application/Customer/Handlers/AddCustomerHandler.cs
+++ src/BugStore.Application/Customer/Handlers/AddCustomerHandler.cs
@@ -1,28 +1,67 @@
 using BugStore.Application.Customer.Commands;
 using BugStore.Application.Customer.DTOs;
+using BugStore.Application.Customer.Validators;
 using BugStore.Domain.Interfaces;
+using BugStore.Exception.ExceptionMessages;
+using BugStore.Exception.ProjectException;
 using Mediator;
 
 namespace BugStore.Application.Customer.Handlers;
+
 public class AddCustomerHandler : IRequestHandler<AddCustomerCommand, ResponseDataCustomerDTO>
 {
-    private readonly ICustomerWriteRepository _repository;
+    private readonly ICustomerReadOnlyRepository _readRepository;
+    private readonly ICustomerWriteRepository _writeRepository;
     private readonly IUnitOfWork _unitOfWork;
 
-    public AddCustomerHandler(ICustomerWriteRepository repository, IUnitOfWork unitOfWork)
+    public AddCustomerHandler(
+        ICustomerReadOnlyRepository readRepository,
+        ICustomerWriteRepository writeRepository,
+        IUnitOfWork unitOfWork
+    )
     {
-        _repository = repository;
+        _readRepository = readRepository;
+        _writeRepository = writeRepository;
         _unitOfWork = unitOfWork;
     }
 
-    public async ValueTask<ResponseDataCustomerDTO> Handle(AddCustomerCommand command, CancellationToken cancellationToken)
+    public async ValueTask<ResponseDataCustomerDTO> Handle(
+        AddCustomerCommand command,
+        CancellationToken cancellationToken
+    )
     {
         var request = command.Request;
-        var customer = new Domain.Entities.Customer(request.Name, request.Email, request.Phone, request.BirthDate);
+        await ValidateAsync(request);
+        var customer = new Domain.Entities.Customer(
+            request.Name,
+            request.Email,
+            request.Phone,
+            request.BirthDate
+        );
 
-        await _repository.AddAsync(customer);
+        await _writeRepository.AddAsync(customer);
         await _unitOfWork.CommitAsync();
 
-        return new ResponseDataCustomerDTO(customer.Id, customer.Name, customer.Email, customer.Phone, customer.BirthDate);
+        return new ResponseDataCustomerDTO(
+            customer.Id,
+            customer.Name,
+            customer.Email,
+            customer.Phone,
+            customer.BirthDate
+        );
+    }
+
+    private async Task ValidateAsync(RequestCustomerDTO request)
+    {
+        var validator = new CustomerValidator();
+        var validationResult = await validator.ValidateAsync(request);
+
+        if (!validationResult.IsValid)
+            throw new OnValidationException(
+                validationResult.Errors.Select(x => x.ErrorMessage).ToList()
+            );
+        var exists = await _readRepository.ExistsByEmailAsync(request.Email);
+        if (exists)
+            throw new ConflitException(ResourceExceptionMessage.EMAIL_ALREADY_REGISTERED);
     }
 }
--- src/BugStore.Application/Customer/Handlers/UpdateCustomerHadler.cs
+++ src/BugStore.Application/Customer/Handlers/UpdateCustomerHadler.cs
@@ -1,5 +1,6 @@
 using BugStore.Application.Customer.Commands;
 using BugStore.Application.Customer.DTOs;
+using BugStore.Application.Customer.Validators;
 using BugStore.Domain.Interfaces;
 using BugStore.Exception.ExceptionMessages;
 using BugStore.Exception.ProjectException;
@@ -7,21 +8,32 @@ using Mediator;
 
 namespace BugStore.Application.Customer.Handlers
 {
-    public class UpdateCustomerHadler : IRequestHandler<UpdateCustomerCommand, ResponseDataCustomerDTO>
+    public class UpdateCustomerHadler
+        : IRequestHandler<UpdateCustomerCommand, ResponseDataCustomerDTO>
     {
         private readonly ICustomerWriteRepository _writeRepository;
         private readonly ICustomerReadOnlyRepository _readOnlyRepository;
         private readonly IUnitOfWork _unitOfWork;
-        public UpdateCustomerHadler(ICustomerWriteRepository writeRepository, ICustomerReadOnlyRepository readOnlyRepository, IUnitOfWork unitOfWork)
+
+        public UpdateCustomerHadler(
+            ICustomerWriteRepository writeRepository,
+            ICustomerReadOnlyRepository readOnlyRepository,
+            IUnitOfWork unitOfWork
+        )
         {
             _writeRepository = writeRepository;
             _readOnlyRepository = readOnlyRepository;
             _unitOfWork = unitOfWork;
         }
 
-        public async ValueTask<ResponseDataCustomerDTO> Handle(UpdateCustomerCommand command, CancellationToken cancellationToken)
+        public async ValueTask<ResponseDataCustomerDTO> Handle(
+            UpdateCustomerCommand command,
+            CancellationToken cancellationToken
+        )
         {
             var request = command.Request;
+            await ValidateAsync(request, command.Id);
+
             var customer = await _readOnlyRepository.GetByIdAsync(command.Id);
             if (customer is null)
                 throw new NotFoundException(ResourceExceptionMessage.CUSTOMER_NOT_FOUND);
@@ -29,7 +41,32 @@ namespace BugStore.Application.Customer.Handlers
             customer.Update(request.Name, request.Email, request.Phone, request.BirthDate);
             await _writeRepository.UpdateAsync(customer);
             await _unitOfWork.CommitAsync();
-            return new ResponseDataCustomerDTO(customer.Id, customer.Name, customer.Email, customer.Phone, customer.BirthDate);
+            return new ResponseDataCustomerDTO(
+                customer.Id,
+                customer.Name,
+                customer.Email,
+                customer.Phone,
+                customer.BirthDate
+            );
+        }
+
+        private async Task ValidateAsync(RequestCustomerDTO request, Guid customerId)
+        {
+            var validator = new CustomerValidator();
+            var validationResult = await validator.ValidateAsync(request);
+
+            if (!validationResult.IsValid)
+                throw new OnValidationException(
+                    validationResult.Errors.Select(x => x.ErrorMessage).ToList()
+                );
+
+            var currentCustomer = await _readOnlyRepository.GetByIdAsync(customerId);
+            if (currentCustomer != null && currentCustomer.Email != request.Email)
+            {
+                var emailExists = await _readOnlyRepository.ExistsByEmailAsync(request.Email);
+                if (emailExists)
+                    throw new ConflitException(ResourceExceptionMessage.EMAIL_ALREADY_REGISTERED);
+            }
         }
     }
 }
