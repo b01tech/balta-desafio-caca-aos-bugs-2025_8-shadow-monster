d841790cf17adaca1a9a7d09660726b16b372edc
b01tech <bruno01.tech@gmail.com>
2025-10-30 22:15:00 -0300

    feat(order): add order line validator with product, quantity and price rules

---
+++ src/BugStore.Application/Order/Validators/OrderLineValidator.cs
@@ -0,0 +1,28 @@
+using BugStore.Application.Order.DTOs;
+using BugStore.Exception.ExceptionMessages;
+using FluentValidation;
+
+namespace BugStore.Application.Order.Validators
+{
+    public class OrderLineValidator : AbstractValidator<RequestOrderLineDTO>
+    {
+        public OrderLineValidator()
+        {
+            RuleFor(x => x.ProductId)
+                .NotEmpty()
+                .WithMessage(ResourceExceptionMessage.PRODUCT_ID_EMPTY);
+
+            RuleFor(x => x.Quantity)
+                .NotEmpty()
+                .WithMessage(ResourceExceptionMessage.QUANTITY_EMPTY)
+                .GreaterThan(0)
+                .WithMessage(ResourceExceptionMessage.QUANTITY_MUST_BE_POSITIVE);
+
+            RuleFor(x => x.Price)
+                .NotEmpty()
+                .WithMessage(ResourceExceptionMessage.ORDER_LINE_PRICE_EMPTY)
+                .GreaterThan(0)
+                .WithMessage(ResourceExceptionMessage.ORDER_LINE_PRICE_INVALID);
+        }
+    }
+}
